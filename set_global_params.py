5253e9bd1b3f39bbb8981840058f75b1133edfab
fgreenstreet <fmjgreesntreet@gmail.com>
2022-10-26 14:06:11 +0100

    fixes to the gitignore

--- set_global_params.py
+++
@@ -1,4 +0,0 @@
-# PATHS
-experiment_record_path = 'W:\\photometry_2AC\\experimental_record.csv'
-processed_data_path = 'W:\\photometry_2AC\\processed_data\\'
-post_processed_tracking_data_path = 'W:\\photometry_2AC\\tracking_analysis\\'
