ef36f78e6d722aee228e1901e22cf4a90c6cb803
fgreenstreet <fmjgreesntreet@gmail.com>
2024-03-13 12:00:58 +0000

    fixed error in number of psychometric session used

--- figure6/reformat_psychometric_data.py
+++ figure6/reformat_psychometric_data.py
@@ -11,20 +11,20 @@ key = 'fitted max cumsum ang vel'
 alignment = 'reward'
 all_trial_data = get_all_psychometric_session_dlc(mouse_ids, site, save=False, load_saved=False, get_movement=False, align_to=alignment, num_sessions=num_sessions)
 save_path = os.path.join(processed_data_path, 'psychometric_data')
-all_trial_data.to_pickle(os.path.join(save_path,"all_trial_data_{}_contra_ipsi_last_trial_confidence_and_traces_no_tracking_{}_aligned_pk5.pkl".format(site, alignment)))
+all_trial_data.to_pickle(os.path.join(save_path, "all_trial_data_{}_contra_ipsi_last_trial_confidence_and_traces_no_tracking_{}_aligned_pk5.pkl".format(site, alignment)))
 
 # first round of tail mice
 site = 'tail'
 mouse_ids = bias_analysis_mice[site][0]
-num_sessions = 2
+num_sessions = 3
 alignment = 'choice'
 all_trial_data = get_all_psychometric_session_dlc(mouse_ids, site, save=False, load_saved=False, get_movement=False, align_to=alignment, num_sessions=num_sessions)
 save_path = os.path.join(processed_data_path, 'psychometric_data')
-all_trial_data.to_pickle(os.path.join(save_path,"all_trial_data_{}_contra_ipsi_last_trial_confidence_and_traces_no_tracking_{}_aligned_old_data_pk5.pkl".format(site, alignment)))
+all_trial_data.to_pickle(os.path.join(save_path, "all_trial_data_{}_contra_ipsi_last_trial_confidence_and_traces_no_tracking_{}_aligned_old_data_pk5.pkl".format(site, alignment)))
 
 # this is for the second round of tail psychometric mice
 mouse_ids = bias_analysis_mice[site][1]
 num_sessions = 4
 all_trial_data = get_all_psychometric_session_dlc(mouse_ids, site, save=False, load_saved=False, get_movement=False, align_to=alignment, num_sessions=num_sessions)
 save_path = os.path.join(processed_data_path, 'psychometric_data')
-all_trial_data.to_pickle(os.path.join(save_path,"all_trial_data_{}_contra_ipsi_last_trial_confidence_and_traces_no_tracking_{}_aligned_pk5.pkl".format(site, alignment)))
+all_trial_data.to_pickle(os.path.join(save_path, "all_trial_data_{}_contra_ipsi_last_trial_confidence_and_traces_no_tracking_{}_aligned_pk5.pkl".format(site, alignment)))
